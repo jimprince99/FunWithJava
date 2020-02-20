1f7995bea7761ea7529eea166f4ec84041730655
jimprince99 <jimprince99@gmail.com>
2020-02-20 13:57:41 +0000

    latest versions

---
+++ FunWithJava/src/com/jimprince99/mbeans/GameMBean.java
@@ -0,0 +1,12 @@
+package com.jimprince99.mbeans;
+
+public interface GameMBean {
+	 
+    public void playFootball(String clubName);
+ 
+    public String getPlayerName();
+ 
+    public void setPlayerName(String playerName);
+    public void playerName(String playerName);
+ 
+}
